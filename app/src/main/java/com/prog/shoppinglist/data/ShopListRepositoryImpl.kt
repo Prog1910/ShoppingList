2146b1b804819254c00e6b4163d7b1e0448aa585
Prog1910 <102432993+Prog1910@users.noreply.github.com>
2022-11-09 15:00:57 +0400

    Created data layer

---
+++ app/src/main/java/com/prog/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,39 @@
+package com.prog.shoppinglist.data
+
+import com.prog.shoppinglist.domain.ShopItem
+import com.prog.shoppinglist.domain.ShopListRepository
+
+object ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (autoIncrementId == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldItem = getShopItem(shopItem.id)
+        shopList.remove(oldItem)
+        addShopItem(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find {
+            it.id == shopItemId
+        } ?: throw RuntimeException("Element with id $shopItemId not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+
+}
