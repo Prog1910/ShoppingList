3af417a72bf23dae69b0f77b88524954422e2d81
Prog1910 <102432993+Prog1910@users.noreply.github.com>
2022-11-09 16:07:59 +0400

    Created ui

--- app/src/main/java/com/prog/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/prog/shoppinglist/presentation/MainActivity.kt
@@ -2,7 +2,6 @@ package com.prog.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
